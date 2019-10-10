dcc297d557cd4ee2e71320b0302632730d8ebca1
nubyia <shareenca@gmail.com>
2019-10-10 00:25:28 -0400

    Ex401

---
+++ src/main/java/com/example/demo/Sb401Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb401Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb401Application.class, args);
+    }
+
+}
